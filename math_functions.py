e01310fd4ea23be5444fc4cfd294c54dc952a220
pvihung <168234422+pvihung@users.noreply.github.com>
2024-11-24 16:04:10 -0800

    Delete math_functions.py

--- math_functions.py
+++
@@ -1,9 +0,0 @@
-import math_functions
-def power(a, b): 
-    return a ** b
-
-def divide(a, b):
-    if b == 0: 
-        # Raise-Bao ra exception 
-        raise ValueError("Can not divide a number by zero")
-    return a/b
